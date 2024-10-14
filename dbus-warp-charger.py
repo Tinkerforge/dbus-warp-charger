fdbc9b18b5ba899b659c7438701eb1d9f55f52a3
Matthias Bolte <matthias@tinkerforge.com>
2024-10-14 18:24:15 +0200

    Whitespace

--- dbus-warp-charger.py
+++ dbus-warp-charger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
- 
+
 # import normal packages
-import platform 
+import platform
 import logging
 import sys
 import os
@@ -14,7 +14,7 @@ import sys
 import time
 import requests # for http GET
 import configparser # for config/ini file
- 
+
 # our own packages from victron
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
 from vedbus import VeDbusService
@@ -36,66 +36,66 @@ class DbusWARPChargerService:
 
         self._dbusservice = VeDbusService("{}.http_{:02d}".format(servicename, deviceinstance))
         self._paths = paths
-        
+
         self.enable_charging = True # start/stop
-        
+
         logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
-        
-        #get general data from WARP-Charger
+
+        # get general data from WARP-Charger
         version = self.getWARPChargerData("/info/version")
         name = self.getWARPChargerData("/info/name")
         displayname = self.getWARPChargerData("/info/display_name")
-        
+
         # Create the management objects, as specified in the ccgx dbus-api document
         self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
         self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
         self._dbusservice.add_path('/Mgmt/Connection', connection)
-        
+
         # Create the mandatory objects
         self._dbusservice.add_path('/DeviceInstance', deviceinstance)
-        self._dbusservice.add_path('/ProductId', 0xFFFF) # 
+        self._dbusservice.add_path('/ProductId', 0xFFFF)
         self._dbusservice.add_path('/ProductName', name["display_type"])
-        self._dbusservice.add_path('/CustomName', displayname["display_name"])    
+        self._dbusservice.add_path('/CustomName', displayname["display_name"])
         self._dbusservice.add_path('/FirmwareVersion', version['firmware'])
         self._dbusservice.add_path('/Serial', name["uid"])
         self._dbusservice.add_path('/HardwareVersion', name["type"])
         self._dbusservice.add_path('/Connected', 1)
         self._dbusservice.add_path('/UpdateIndex', 0)
         self._dbusservice.add_path('/Position', self.acposition) # 0: ac out, 1: ac in
-        
+
         # add path values to dbus
         for path, settings in self._paths.items():
             self._dbusservice.add_path(path, settings['initial'], gettextcallback=settings['textformat'], writeable=True, onchangecallback=self._handlechangedvalue)
 
         # last update
         self._lastUpdate = 0
-        
+
         # charging time in float
         self._chargingTime = 0.0
 
         # add _update function 'timer'
         gobject.timeout_add(250, self._update) # pause 250ms before the next request
-        
+
         # add _signOfLife 'timer' to get feedback in log every 5minutes
         gobject.timeout_add(self._getSignOfLifeInterval()*60*1000, self._signOfLife)
-   
+
     def _getConfig(self):
         config = configparser.ConfigParser()
         config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
         return config
-   
-   
+
+
     def _getSignOfLifeInterval(self):
         config = self._getConfig()
         value = config['DEFAULT']['SignOfLifeLog']
-        
-        if not value: 
+
+        if not value:
             value = 0
-        
+
         return int(value)
-   
+
+
     def _handlechangedvalue(self, path, value):
-        
         logging.critical("someone else updated %s to %s" % (path, value))
 
         if path == '/MaxCurrent':
@@ -104,7 +104,7 @@ class DbusWARPChargerService:
             pass # ignore, only maxCurrent Implemented
         elif path == '/AutoStart':
             if value == 0:
-                self.setWARPChargerValue("/evse/auto_start_charging", {"auto_start_charging": "false" })
+                self.setWARPChargerValue("/evse/auto_start_charging", {"auto_start_charging": "false"})
             else:
                 self.setWARPChargerValue("/evse/auto_start_charging", {"auto_start_charging": "true"})
         elif path == '/Mode':
@@ -114,7 +114,7 @@ class DbusWARPChargerService:
                 self.setWARPChargerValue("/power_manager/charge_mode", {"mode": 2}) # pv
             elif value == 2:
                 pass # schedule not implemented
-            
+
         elif path == '/StartStop':
             if value == 0:
                 self.setWARPChargerValue("/evse/stop_charging", {})
@@ -122,45 +122,44 @@ class DbusWARPChargerService:
             else:
                 self.setWARPChargerValue("/evse/start_charging", {})
                 self.enable_charging = True
-    
-    
+
+
     def setWARPChargerValue(self, path, dat):
         try:
             request_data = requests.put(url = "http://" + self.ip + path, json = dat, timeout=5)
         except Exception:
             logging.critical('Error at %s', '_update', exc_info=e)
-        
-   
+
+
     def getWARPChargerData(self, path):
         URL = "http://" + self.ip + path
         try:
             request_data = requests.get(url = URL, timeout=5)
         except Exception:
             return None
-        
+
         # check for response
         if not request_data:
             raise ConnectionError("No response from WARP Charger - %s" % (URL))
-        
-        json_data = request_data.json()     
-        
+
+        json_data = request_data.json()
+
         # check for Json
         if not json_data:
             raise ValueError("Converting response to JSON failed")
-      
+
         return json_data
-   
-   
+
     def _signOfLife(self):
         logging.info("--- Start: sign of life ---")
         logging.info("Last _update() call: %s" % (self._lastUpdate))
         logging.info("Last '/Ac/Power': %s" % (self._dbusservice['/Ac/Power']))
         logging.info("--- End: sign of life ---")
         return True
-   
-    def _update(self):   
+
+    def _update(self):
         try:
-            #read out meter values (only WARP Pro Charger)
+            # read out meter values (only WARP Pro Charger)
             data = self.getWARPChargerData("/meters/0/config")
             if data is not None and data[1] != None:
                 # read meter data
@@ -177,7 +176,7 @@ class DbusWARPChargerService:
                     self._dbusservice['/Current'] = float(data[19])
                     
                     
-            # read mode stuff        
+            # read mode stuff
             pmcm = self.getWARPChargerData("/power_manager/charge_mode")
             if pmcm["mode"] == 0: # fast
                 self._dbusservice['/Mode'] = 0 # manual
@@ -186,24 +185,24 @@ class DbusWARPChargerService:
                 self._dbusservice['/StartStop'] = 0 # disabled
             elif pmcm["mode"] == 2 or pmcm["mode"] == 3: # pv / pv+min
                 self._dbusservice['/Mode'] = 1 # automatic
-                    
-            # read all other data         
+
+            # read all other data
             cc = self.getWARPChargerData("/charge_tracker/current_charge")
-            es = self.getWARPChargerData("/evse/state")    
+            es = self.getWARPChargerData("/evse/state")
             ell = self.getWARPChargerData("/evse/low_level_state")
             esl = self.getWARPChargerData("/evse/slots")
-            
-            #self._dbusservice['/MaxCurrent'] = float(es["allowed_charging_current"]/1000.0) # will be set to 0A e.g. by load management
-            self._dbusservice['/MaxCurrent'] = float(esl[8]["max_current"]/1000.0) # external config slot
-            
+
+            #self._dbusservice['/MaxCurrent'] = float(es["allowed_charging_current"] / 1000.0) # will be set to 0A e.g. by load management
+            self._dbusservice['/MaxCurrent'] = float(esl[8]["max_current"] / 1000.0) # external config slot
+
             if es is not None and ell is not None:
                 if es["charger_state"] == 3:
-                    self._dbusservice['/ChargingTime'] = int(ell["time_since_state_change"]/1000.0)
+                    self._dbusservice['/ChargingTime'] = int(ell["time_since_state_change"] / 1000.0)
                     self._dbusservice['/Ac/Energy/Forward'] = float(data[36]-cc["meter_start"]) # calculate charged energy
                 else:
                     self._dbusservice['/ChargingTime'] = 0
                     self._dbusservice['/Ac/Energy/Forward'] = 0.0 # calculate charged energy
-            
+
             if es["charger_state"] == 0: # not connected
                 self._dbusservice['/Status'] = 0 # not connected
             elif es["charger_state"] == 1: # connected - wait for ok
@@ -231,26 +230,26 @@ class DbusWARPChargerService:
                 self._dbusservice['/AutoStart'] = 1
             else:
                 self._dbusservice['/AutoStart'] = 0
-                
+
             if not self.enable_charging:
                 self._dbusservice['/StartStop'] = 0
             else:
                 self._dbusservice['/StartStop'] = 1
-            
-            #logging
+
+            # logging
             logging.debug("Wallbox Consumption (/Ac/Power): %s" % (self._dbusservice['/Ac/Power']))
             logging.debug("Wallbox Forward (/Ac/Energy/Forward): %s" % (self._dbusservice['/Ac/Energy/Forward']))
             logging.debug("---")
-            
+
             # increment UpdateIndex - to show that new data is available
             index = self._dbusservice['/UpdateIndex'] + 1  # increment index
             if index > 255:   # maximum value of the index
                 index = 0       # overflow from 255 to 0
             self._dbusservice['/UpdateIndex'] = index
 
-            #update lastupdate vars
-            self._lastUpdate = time.time()  
-            
+            # update lastupdate vars
+            self._lastUpdate = time.time()
+
             #TODO: missing values
             """
             '/PCB/Temperature',
@@ -270,13 +269,13 @@ class DbusWARPChargerService:
         except Exception as e:
             logging.critical('Error at %s', '_update', exc_info=e)
             logging.critical(e)
-         
-      # return true, otherwise add_timeout will be removed from GObject - see docs http://library.isr.ist.utl.pt/docs/pygtk2reference/gobject-functions.html#function-gobject--timeout-add
+
+        # return true, otherwise add_timeout will be removed from GObject - see docs http://library.isr.ist.utl.pt/docs/pygtk2reference/gobject-functions.html#function-gobject--timeout-add
         return True
-   
+
 
 def main():
-  #configure logging
+    # configure logging
     logging.basicConfig(  format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                           datefmt='%Y-%m-%d %H:%M:%S',
                           level=logging.INFO,
@@ -285,15 +284,15 @@ def main():
                               logging.StreamHandler()
                           ]
                         )
-   
+
     try:
         logging.info("Start")
 
         from dbus.mainloop.glib import DBusGMainLoop
         # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
         DBusGMainLoop(set_as_default=True)
-       
-        #formatting 
+
+        # formatting
         _kwh = lambda p, v: (str(round(v, 2)) + 'kWh')
         _a = lambda p, v: (str(round(v, 1)) + 'A')
         _w = lambda p, v: (str(round(v, 1)) + 'W')
@@ -301,9 +300,8 @@ def main():
         _degC = lambda p, v: (str(v) + '°C')
         _s = lambda p, v: (str(v) + 's')
         _n = lambda p, v: (str(v))
-        
-       
-        #start our main-service
+
+        # start our main-service
         pvac_output = DbusWARPChargerService(
           servicename='com.victronenergy.evcharger',
           paths={
@@ -314,24 +312,24 @@ def main():
             '/Ac/Voltage': {'initial': 0, 'textformat': _v},
             '/Ac/Frequency': {'initial': 0, 'textformat': _v},
             '/Ac/Energy/Forward': {'initial': 0, 'textformat': _kwh},
-            
+
             '/Current': {'initial': 0, 'textformat': _a},
             '/MaxCurrent': {'initial': 0, 'textformat': _a},
             '/SetCurrent': {'initial': 0, 'textformat': _a},
-            
+
             '/AutoStart': {'initial': 0, 'textformat': _n},
             '/ChargingTime': {'initial': 0, 'textformat': _s},
             '/Mode': {'initial': 0, 'textformat': _n},
             '/StartStop': {'initial': 0, 'textformat': _n},
             '/Status': {'initial': 0, 'textformat': _n},
           }
-          )
-       
+        )
+
         logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
         mainloop = gobject.MainLoop()
-        mainloop.run()            
+        mainloop.run()
     except Exception as e:
-      logging.critical('Error at %s', 'main', exc_info=e)
-  
+        logging.critical('Error at %s', 'main', exc_info=e)
+
 if __name__ == "__main__":
     main()
